792cda8312a13e673f06ec54d7058fb72f9ea13f
Suzy-Lana777 <reutasvetlana382@gmail.com>
2026-03-10 16:40:02 +0200

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 1. Статичні імпорти (тепер вони завантажуються відразу)
+import HomePage from './pages/HomePage/HomePage';
+import NewsPage from './pages/NewsPage/NewsPage';
+import NoticesPage from './pages/NoticesPage/NoticesPage';
+import FriendsPage from './pages/FriendsPage/FriendsPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/news" element={<NewsPage />} />
+      <Route path="/notices" element={<NoticesPage />} />
+      <Route path="/friends" element={<FriendsPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      
+      {/* 404 сторінка */}
+      <Route path="/404" element={<NotFoundPage />} />
+      <Route path="*" element={<Navigate to="/404" />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <section>
+      <h1>Вітаємо в PetLove!</h1>
+      <p>Тут буде головна сторінка з песиками.</p>
+    </section>
+  );
+}
