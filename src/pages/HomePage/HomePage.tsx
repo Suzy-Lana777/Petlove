4fe1bb28cb132a0d722d9e24483c8ac2c393f696
Suzy-Lana777 <reutasvetlana382@gmail.com>
2026-03-18 17:12:34 +0200

    home page img

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,29 @@
+// TypeScript більше не сваритиметься, бо імпорт прибрано
+import styles from "./Home.module.scss";
+
 export default function HomePage() {
   return (
-    <section>
-      <h1>Вітаємо в PetLove!</h1>
-      <p>Тут буде головна сторінка з песиками.</p>
+    <section className={styles.heroSection}>
+      <div className="container">
+        
+        {/* Жовта картка з текстом (декор, фон) */}
+        <div className={styles.heroContent}>
+          <div className={styles.textContent}>
+            <h1 className={styles.title}>
+              Take good care of your <span>small pets</span>
+            </h1>
+            <p className={styles.description}>
+              Choosing a pet for your home is a choice that is as important as 
+              choosing a family member.
+            </p>
+          </div>
+        </div>
+
+        {/* Блок із ФОНОВИМ зображенням */}
+        <div className={styles.heroBackgroundBlock}>
+          {/* Він порожній, бо картинка задається в CSS */}
+        </div>
+      </div>
     </section>
   );
 }
